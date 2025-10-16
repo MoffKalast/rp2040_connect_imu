5a7371a6a9054f875cfc336b0fe7e401a9982057
MoffKalast <vid.rijavec@gmail.com>
2025-10-16 13:04:18 +0200

    raw mode firmware and driver

--- firmware/uart.py
+++ firmware/uart.py
@@ -4,22 +4,16 @@ import struct
 import math
 
 from machine import UART, Timer, Pin
-from lib_imu_quat import Imu
+from lib_imu import Imu
 
 class PacketSerial:
 	def __init__(self):
 		self.imu = Imu()
 		self.imu_timer = Timer(period=10, mode=Timer.PERIODIC, callback=self.send_imu_data)
-		#self.sending_prescaler = 0
 		
 	def send_imu_data(self, timer):
-		self.imu.update()
-	
-		#50 hz
-		#if self.sending_prescaler == 0:
-		print(self.imu.get_printout())
-	
-		#self.sending_prescaler = (self.sending_prescaler  + 1) % 4
+		#100 hz
+		print(self.imu.read())
 
 	def cleanup(self):
 		self.imu_timer.deinit()
--- src/imu.py
+++ src/imu.py
@@ -19,8 +19,7 @@ class RP2040IMU:
 
 		self.connect_to_serial_port()
 
-		self.imu_pub = rospy.Publisher("/rp2040_imu/data", Imu, queue_size=10)
-		self.pose_pub = rospy.Publisher("/rp2040_imu/pose", PoseStamped, queue_size=10)
+		self.imu_pub = rospy.Publisher("/rp2040_imu/data_raw", Imu, queue_size=10)
 
 		rospy.loginfo("IMU Ready")
 
@@ -36,21 +35,21 @@ class RP2040IMU:
 				print("Retrying in 2 seconds...")
 				time.sleep(2)  # Wait for 2 seconds before retrying
 
-	def publish_imu_msg(self, x, y, z, w, gx, gy, gz, ax, ay, az):
+	def publish_imu_msg(self, ax, ay, az, gx, gy, gz):
 
 		imu_msg = Imu()
 		imu_msg.header.stamp = rospy.Time.now()
-		imu_msg.header.frame_id = "imu_link"
+		imu_msg.header.frame_id = "rp2040_imu_link"
 
 		# Orientation (quaternion)
-		imu_msg.orientation.x = x
-		imu_msg.orientation.y = y
-		imu_msg.orientation.z = z
-		imu_msg.orientation.w = w
+		imu_msg.orientation.x = 0
+		imu_msg.orientation.y = 0
+		imu_msg.orientation.z = 0
+		imu_msg.orientation.w = 0
 		imu_msg.orientation_covariance = [
-			0.0025, 0, 0,
-			0, 0.0025, 0,
-			0, 0, 0.0025
+			9999, 0, 0,
+			0, 9999, 0,
+			0, 0, 9999
 		]
 
 		# Angular velocity (rad/s)
@@ -74,44 +73,26 @@ class RP2040IMU:
 		]
 		self.imu_pub.publish(imu_msg)
 
-		pose_msg = PoseStamped()
-		pose_msg.header.stamp = rospy.Time.now()
-		pose_msg.header.frame_id = "imu_link"
-		pose_msg.pose.position.x = 0
-		pose_msg.pose.position.y = 0
-		pose_msg.pose.position.z = 0
-		pose_msg.pose.orientation.x = x
-		pose_msg.pose.orientation.y = y
-		pose_msg.pose.orientation.z = z
-		pose_msg.pose.orientation.w = w
-		self.pose_pub.publish(pose_msg)
-
 	def receive_data(self):
-		while not rospy.is_shutdown():
-			try:
-				message = self.port.readline().decode("utf-8","ignore").strip()
-				if message.startswith('#') and message.endswith('#'):
-					split = message.replace("#","").split(",")
-					if len(split) == 10:
-						self.publish_imu_msg(
-							float(split[0]), #x
-							float(split[1]), #y
-							float(split[2]), #z
-							float(split[3]), #w
+		try:
+			message = self.port.readline().decode("utf-8","ignore").strip()
+			if message.startswith('#') and message.endswith('#'):
+				split = message.replace("#","").split(",")
+				if len(split) == 6:
+					self.publish_imu_msg(
+						float(split[0]), #raw accel x
+						float(split[1]), #raw accel y
+						float(split[2]),  #raw accel z
 
-							float(split[4]), #raw gyro x
-							float(split[5]), #raw gyro y
-							float(split[6]), #raw gyro z
-
-							float(split[7]), #raw accel x
-							float(split[8]), #raw accel y
-							float(split[9])  #raw accel z
-						)
-			except (serial.SerialException, OSError) as e:
-				print(f"Connection lost: {e}")
-				print("Attempting to reconnect...")
-				self.port.close()  # Close the port if it is open
-				self.connect_to_serial_port()  # Attempt to reconnect
+						float(split[3]), #raw gyro x
+						float(split[4]), #raw gyro y
+						float(split[5]) #raw gyro z
+					)
+		except (serial.SerialException, OSError) as e:
+			print(f"Connection lost: {e}")
+			print("Attempting to reconnect...")
+			self.port.close()  # Close the port if it is open
+			self.connect_to_serial_port()  # Attempt to reconnect
 
 
 
@@ -119,10 +100,7 @@ class RP2040IMU:
 		self.port.close()
 
 imu = RP2040IMU()
-
-rate = rospy.Rate(rospy.get_param('rate', 60))
 rospy.on_shutdown(imu.cleanup)
 
 while not rospy.is_shutdown():
 	imu.receive_data()
-	rate.sleep()
